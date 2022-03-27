d15c65a90ccc7d8a8a3c92d80bbe970ee0e317a0
mohammad yadegari <mohammadyadegari032@gmail.com>
2022-03-27 13:55:25 +0430

    show meshes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Engine, FreeCamera, Scene, SceneLoader, Vector3 } from 'babylonjs';
+import 'babylonjs-loaders';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,30 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const canvas = document.getElementById('view') as HTMLCanvasElement;
+    const engine = new Engine(canvas, true);
+    const scene = new Scene(engine);
+
+    //define free camera
+    const camera = new FreeCamera("camera", new Vector3(0, 0, 0), scene);
+    camera.attachControl(canvas, true);
+
+    //import room model
+    SceneLoader.ImportMeshAsync('', '/assets/3d models/room.glb').then(() => {
+      
+    });
+
+
+    //import books
+    SceneLoader.ImportMeshAsync('', '/assets/3d models/books-1.glb').then(() => {
+
+    });
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    })
   }
+
+
 
 }
