3576f3fae26d9f96cc1111dd5827777bedea4730
mohammad yadegari <mohammadyadegari032@gmail.com>
2022-06-10 02:22:18 +0430

    add simple action for books

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { Engine, FreeCamera, HDRCubeTexture, HemisphericLight, Scene, SceneLoader, Vector3 } from 'babylonjs';
+import { AbstractMesh, ActionManager, Color3, Engine, ExecuteCodeAction, FreeCamera, HDRCubeTexture, HemisphericLight, Scene, SceneLoader, TransformNode, Vector3 } from 'babylonjs';
 import 'babylonjs-loaders';
+
+type Book = {
+  nodeId: string,
+  title: string,
+  description: string,
+  meshes?: AbstractMesh[],
+}
+
+const BROWSER_MESH_ACTION_MAP = {
+  onMouseEnter: 'OnPointerOverTrigger',
+  onMouseLeave: 'OnPointerOutTrigger',
+  onMouseDown: 'OnPickDownTrigger',
+  onMouseUp: 'OnPickUpTrigger',
+  onClick: 'OnPickTrigger',
+  onDoubleClick: 'OnDoublePickTrigger'
+} as any;
+const BROWSER_MESH_EVENT_MAP = {
+  onLoad: 'onReady'
+} as any;
+const MESH_OUTLINE_COLOR = "#FFFFFF";
+const MESH_OVERLAY_COLOR = "#999999";
+
+var books: Book[] = [
+  {
+    nodeId: 'Book-1',
+    title: 'book 1',
+    description: 'it\'s book 1'
+  },
+  {
+    nodeId: 'Book-2',
+    title: 'book 2',
+    description: 'it\'s book 2'
+  },
+  {
+    nodeId: 'Book-3',
+    title: 'book 3',
+    description: 'it\'s book 3'
+  },
+  {
+    nodeId: 'Book-4',
+    title: 'book 4',
+    description: 'it\'s book 4'
+  },
+  {
+    nodeId: 'Book-5',
+    title: 'book 5',
+    description: 'it\'s book 6'
+  },
+  {
+    nodeId: 'Book-7',
+    title: 'book 7',
+    description: 'it\'s book 7'
+  }
+]
 
 @Component({
   selector: 'app-home',
@@ -40,15 +94,61 @@ export class HomeComponent implements OnInit {
 
 
     //import books
-    SceneLoader.ImportMeshAsync('', '/assets/3d models/books-1.glb').then(() => {
+    SceneLoader.ImportMeshAsync('', '/assets/3d models/books-1.glb').then((result) => {
+      books.forEach(e => {
+        e.meshes = (scene.getNodeByID(e.nodeId) as TransformNode)?.getChildMeshes() as AbstractMesh[];
+      });
 
+      registerAppliencesActions();
     });
 
     engine.runRenderLoop(() => {
       scene.render();
     })
+
+
+    function registerAppliencesActions() {
+      books.forEach((book, index) => {
+        const itemEventHandlers = [
+          {
+            name: 'onMouseEnter', handler: (book: Book) =>
+              book.meshes?.forEach(mesh => {
+                mesh.renderOutline = true
+              })
+          },
+          {
+            name: 'onMouseLeave', handler: (appliance: Book) =>
+              book.meshes?.forEach(mesh => {
+                mesh.renderOutline = false
+              })
+          }
+        ];
+
+        attachItemEventHandlers(book, itemEventHandlers);
+      });
+    }
+
+    const attachItemEventHandlers = (book: Book, events: { name: string, handler: (book: Book) => void }[]) => {
+      var outlineWidth = 0.005;
+
+      book.meshes?.forEach(mesh => {
+
+        mesh.overlayColor = Color3.FromHexString(MESH_OVERLAY_COLOR);
+        mesh.outlineColor = Color3.FromHexString(MESH_OUTLINE_COLOR);
+        mesh.outlineWidth = outlineWidth;
+        mesh.overlayAlpha = 1;
+        mesh.actionManager = new ActionManager(scene);
+
+
+        events.forEach(({ name, handler }) => {
+          const eventName: string = BROWSER_MESH_ACTION_MAP[name] || BROWSER_MESH_EVENT_MAP[name];
+          const action = new ExecuteCodeAction(
+            (ActionManager as any)[eventName],
+            () => handler(book)
+          );
+          mesh.actionManager?.registerAction(action);
+        });
+      });
+    }
   }
-
-
-
 }
