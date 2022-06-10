88a234f7440a341edd6340d8e5ef57a44c1fe081
mohammad yadegari <mohammadyadegari032@gmail.com>
2022-06-10 18:33:12 +0430

    add book image to book detail card

--- src/app/book-detail/book-detail-slider/book-detail-slider.component.ts
+++ src/app/book-detail/book-detail-slider/book-detail-slider.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-book-detail-slider',
+  selector: 'app-book-detail-slider[imageUrl]',
   templateUrl: './book-detail-slider.component.html',
   styleUrls: ['./book-detail-slider.component.scss']
 })
 export class BookDetailSliderComponent implements OnInit {
 
   constructor() { }
+
+  @Input() imageUrl: string = "";
+  @Input() alt: string = "";
 
   ngOnInit(): void {
   }
