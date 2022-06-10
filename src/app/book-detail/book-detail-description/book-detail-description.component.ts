af96aa94bfbe342ef8e6d9c8c2b2b35d63aa5bc8
mohammad yadegari <mohammadyadegari032@gmail.com>
2022-06-10 22:25:30 +0430

    add book description

--- src/app/book-detail/book-detail-description/book-detail-description.component.ts
+++ src/app/book-detail/book-detail-description/book-detail-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-detail-description',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class BookDetailDescriptionComponent implements OnInit {
 
   constructor() { }
+
+  @Input() data: string = '';
 
   ngOnInit(): void {
   }
