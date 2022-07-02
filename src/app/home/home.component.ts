d82b3b01c65ff1efffe6c5c3f4f65b7edd83b293
mohammad yadegari <mohammadyadegari032@gmail.com>
2022-07-02 14:06:42 +0430

    show and hidd bookdetail card with click on books

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractMesh, ActionManager, Color3, CubicEase, Engine, ExecuteCodeAction, FreeCamera, HDRCubeTexture, HemisphericLight, HighlightLayer, Mesh, MeshBuilder, Scene, SceneLoader, TransformNode, Vector3 } from 'babylonjs';
 import 'babylonjs-loaders';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BookDetailInformation, BookDetailInput } from '../book-detail/book-detail.component';
 
 type Book = {
   nodeId: string,
@@ -78,8 +80,33 @@ var books: Book[] = [
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  bookDetailInput: BookDetailInput = { informationData: [], description: '' };
+  showBookDetail$: Observable<boolean> = new Observable<boolean>();
+  showBookDetailSubject = new Subject<boolean>();
 
-  constructor() { }
+  constructor() {
+    this.bookDetailInput.
+      informationData = [
+        { title: 'کد کالا:', value: '32643' },
+        { title: 'شابک:', value: '9789643113445' },
+        { title: 'نویسنده :', value: 'عباس معروف' },
+        { title: 'انتشارات :', value: 'ققنوس' },
+        { title: 'موضوع :', value: 'داستان های فارسی قرن 14' },
+        { title: 'زبان :', value: 'فارسي' },
+        { title: 'جلد :', value: 'نرم' },
+        { title: 'قطع :', value: 'رقعی' },
+        { title: 'تعداد صفحه :', value: '350' },
+        { title: 'طول :', value: '21' },
+        { title: 'عرض :', value: '14' },
+        { title: 'وزن :', value: '392 گرم' },
+      ]
+
+    this.bookDetailInput.
+      description = "درباره کتاب: سمفونی مردگان داستان زندگی یک خانواده است که در اردبیل در اواخر سلطنت رضا شاه زندگی می کنند. کتاب از ۴ فصل تشکیل شده که هر فصل از زبان یک راوی نقل می شود. پدر خانواده «جابر اورخانی» مغازه آجیل و خشکبار فروشی در بازار اردبیل دارد. بچه هایش به ترتیب یوسف، آیدین و آیدا ، و اورهان هستند که دخترش آیدا، با آیدین دوقلوست. اورهان که بیشتر از سایرین به پدر در اداره مغازه کمک می کند، مورد علاقه اوست و مادر آیدین را بیش از فرزندان دیگرش دوست دارد. آیدین علاقه بسیاری به درس و مطالعه دارد و اورهان بیشتر به کسب و کار علاقمند است. منزل خانواده اورهانی نزدیک پنکه سازی لرد قراردارد. با اوج گیری جنگ جهانی دوم، دولت روس به بخش های شمالی کشور از جمله آذربایجان حمله هوایی می کند و چتر باز پیاده می کند. این مسئله موجب بروز هرج و مرج در شهر می شود. آشفتگی و ناامنی و کمبود ارزاق، بویژه نان در شهر حاکم می شود. جابر دوستی دارد به نام ایاز که پاسبان است. جابر در بیشتر امور زندگی ایاز را طرف شور و مشورت قرار می دهد. یوسف که در این زمان یک پسر بچه است، تحت تاثیر فضای جنگی حاکم بر شهر در خانه، ادای سربازها را در می آورد مخصوصاً محو تماشای چتر بازها می شود. یک روز هم به تقلید از چتر بازها، چتر بزرگ و سیاه پدر را برمی دارد و با آن از بام خانه پرواز می کند. اما در اثر سقوط از پشت بام، یوسف به نوعی عقب افتادگی دچار می شود. قوای تکلم و شنوایی اش به مرور از بین می رود و چیزی می شود بین آدم و حیوان که مدام می بلعد و خود را کثیف می کند . ایاز که به شدت طرفدار رضا شاه است از کناره گیری او از سلطنت به نفع پسرش، به گریه می افتد و جابر را نیز به گریه می اندازد. در خانواده، اورهان و یوسف کم کم با پدر در اداره مغازه کمک می کنند، اما آیدین علاقه ای به کار آجیل فروشی ندارد. نثر داستان خیلی روان و داستانش خیلی منظم است. داستان کتاب آنقدر جذاب است که اصلا دوست نداری کتاب رو زمین بذاری و استراحت کنی. به دوستان عزیز توصیه می کنم اگه وقتشو داشتند حتماً این کتاب رو بخونند. عباس معروفی در سال ۱۳۳۶ شمسی در تهران متولد شد. فارغ التحصیل هنرهای زیبای تهران در رشته هنرهای دراماتیک است و حدود یازده سال معلم ادبیات در دبیرستان های تهران بوده است.معروفی به خاطر موضع گیری علیه حکومت ایران بارها بازجویی شد و سرانجام تحت فشار سیاسی از ایران خارج شد و به آلمان رفت و هم اکنون ساکن برلین است. سمفونی مردگان برنده جایزه «بنیاد انتشارات ادبی فلسفی سور کامپ» در سال ۲۰۰۱ شده و به دو زبان انگلیسی و نیز آلمانی ترجمه و چاپ شده است. ضمن اینکه در سال ۲۰۰۷ به عنوان یکی از صد رمان برجسته سال بریتانیا انتخاب شد.";
+
+    this.showBookDetailSubject.next(false);
+    this.showBookDetail$ = this.showBookDetailSubject.asObservable();
+  }
 
   ngOnInit(): void {
     const canvas = document.getElementById('view') as HTMLCanvasElement;
@@ -116,7 +143,32 @@ export class HomeComponent implements OnInit {
         b.paperMesh = result.meshes.find(m => m.id === b.paperMeshId);
       });
 
-      registerAppliencesActions();
+      // registerAppliencesActions();
+      books.forEach((book, index) => {
+        const itemEventHandlers = [
+          {
+            name: 'onMouseEnter', handler: (book: Book) =>
+              hl.addMesh(book.coverMesh as Mesh, new Color3(250, 250, 250))
+          },
+          {
+            name: 'onMouseLeave', handler: (appliance: Book) =>
+              hl.removeMesh(book.coverMesh as Mesh)
+          },
+          {
+            name: 'onClick', handler: (appliance: Book) => {
+              zoom(camera, appliance.coverMesh!, 2.6, 0.5, undefined);
+              this.showBookDetailSubject.next(true);
+            }
+          },
+          {
+            name: 'onDoubleClick', handler: () => {
+              this.showBookDetailSubject.next(false);
+            }
+          }
+        ];
+
+        attachItemEventHandlers(book, itemEventHandlers);
+      });
     });
 
     engine.runRenderLoop(() => {
@@ -136,27 +188,6 @@ export class HomeComponent implements OnInit {
     });
     hl.outerGlow = false;
     hl.innerGlow = true;
-
-    function registerAppliencesActions() {
-      books.forEach((book, index) => {
-        const itemEventHandlers = [
-          {
-            name: 'onMouseEnter', handler: (book: Book) =>
-              hl.addMesh(book.coverMesh as Mesh, new Color3(250, 250, 250))
-          },
-          {
-            name: 'onMouseLeave', handler: (appliance: Book) =>
-              hl.removeMesh(book.coverMesh as Mesh)
-          },
-          {
-            name: 'onClick', handler: (appliance: Book) =>
-              zoom(camera, appliance.coverMesh!, 2.6, 0.5, undefined)
-          }
-        ];
-
-        attachItemEventHandlers(book, itemEventHandlers);
-      });
-    }
 
     const attachItemEventHandlers = (book: Book, events: { name: string, handler: (book: Book) => void }[]) => {
       var outlineWidth = 0.005;
@@ -219,5 +250,14 @@ export class HomeComponent implements OnInit {
       var globalWalkAnimatable = BABYLON.Animation.CreateAndStartAnimation('cwalk', cam as any, 'position', speed1, tf, cam.position, movePosition, 0, ease as any); // Move to target
       var globalCtargetAnimatable = BABYLON.Animation.CreateAndStartAnimation('ctarget', cam as any, 'target', speed2, tf, cam.target, rotatePosition, 0, ease as any); // Rotate to target
     };
+
+  }
+
+  showBookDetail(): void {
+    this.showBookDetailSubject.next(true);
+  }
+
+  closeBookDetailCard(): void {
+    this.showBookDetailSubject.next(false);
   }
 }
